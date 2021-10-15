aa6fbd721f5751d0ce7cc2c5d645daddcf95e72f
mandersch <malte.k.andersch@gmail.com>
2021-10-15 15:06:30 +0200

    Add documentation for all methods, classes and constants, refactor some methods a little bit

--- lib/registerbot/basic_processor.rb
+++ lib/registerbot/basic_processor.rb
@@ -1,20 +1,42 @@
 require 'telegram/bot'
 
+# The superclass for all used content processors. Defines the most basic functionality for all processors, such as sending messages.
 class Basic_processor
+
+    # Makes the user_state of this class and all subclasses accessible to the bot from outside the processors
+    # @note This method may modify our application state 
     attr_reader :user_state
+
+    # Creates a new `Basic_processor` instance
+    #
+    # The `user_state` instance variable is only ever set to an empty `Hash`, as this class contains no functionality based on the `user_state`
+    # @param bot [Telegram::Bot::Client] the actual bot communicating with the telegram bot-API
     def initialize(bot)
         @user_state = Hash.new()
         @bot = bot
     end
 
+    # Send a Message as a reply to a user's message by letting the bot call the Telegram Bot-API
+    #
+    # @param text [String] The answer text to send to the user
+    # @param message [Telegram::Bot::Types::Message] the original message received by the bot, which should be answered to
     def reply(text, message)
         @bot.api.send_message(chat_id: message.chat.id, text: text, reply_to_message: message)    
     end
 
+    # Send a message to a user by letting the bot call the Telegram Bot-API
+    #
+    # @param text [String] The answer text to send to the user
+    # @param message [Telegram::Bot::Types::Message] the original message received by the bot, used to identify the correct `chat_id` to respond to
     def send_message(text, message)
         @bot.api.send_message(chat_id: message.chat.id, text: text)    
     end
 
+    # Send a message to a user by letting the bot call the Telegram Bot-API
+    #
+    # @param text [String] The answer text to send to the user
+    # @param message [Telegram::Bot::Types::Message] the original message received by the bot, used to identify the correct `chat_id` to respond to
+    # @param markup [Telegram::Bot::Types::ReplyKeyboardMarkup] the markup to be used on a Message e.g. a custom Keyboard, a forced reply, or a revoke-of-custom-keyboard
     def send_message_with_markup (text, message, markup)
         @bot.api.send_message(chat_id: message.chat.id, text: text, reply_markup: markup)
     end
--- lib/registerbot/help_processor.rb
+++ lib/registerbot/help_processor.rb
@@ -2,15 +2,27 @@ require './lib/registerbot/basic_processor'
 require './lib/registerbot/commands'
 require 'telegram/bot'
 
+# A Processor for `message`s that are only for user aid and to not require any knowledge of the `feedback` or `report` database.
+#   Inherits from `Basic_processor` class (see #Basic_processor)
 class Help_processor < Basic_processor
+    
+    # Is called when the user first interacts with the bot and is automatically sending the '/start' command
+    #
+    # @param message [Telegram::Bot::Types::Message] the original message received by the bot, used to identify the correct `chat_id` to respond to
     def start(message)
         send_message("Hallo #{message.from.first_name}! Ich bin ein Bot für Meldungen rechter Aktivitäten in Berlin Charlottenburg-Wilmersdorf. Wenn du mehr über das Register erfahren möchtest, dann schau doch mal auf die Website: https://berliner-register.de/charlottenburg-wilmersdorf.\n\nBei mir kannst du ganz einfach per Telegram-Nachricht Meldungen durchgeben, ohne erst viel auf Websiten oder E-Mails rumsuchen zu müssen. Probier doch glech einmal den /hilfe Befehl, um zu sehen, was ich alles kann.\n\nHinweis: Ich bin kein offizielles Produkt des Berliner Registers, sondern ein privates Projekt zur Unterstützung desselben. Ich befinde mich aktuell noch sehr am Anfang meiner Entwicklung, also entschuldige bitte den kleinen Funktionsumfang und einzelne Bugs.", message)
     end
 
+    # Is called when the user sends the '/hilfe' command, displays all available commands to the user
+    #
+    # @param message [Telegram::Bot::Types::Message] the original message received by the bot, used to identify the correct `chat_id` to respond to
     def help(message)
         send_message("Folgende Befehle kannst du aktuell benutzen:\n\n#{START}: Zeigt die Begrüßungsnachricht an.\n\n#{HELP}: Zeigt alle Verfügbaren Befehle an.\n\n#{FORMULAR_REPORT}: Ein Step-by-Step Formular, um super einfach Meldungen zu machen, ohne etwas zu vergessen.\n\n#{REPORT} <Meldung(en)>: Gib mir neue Meldungen an. Verwende dabei folgendes Format: <Datum>; <Ort>; <Beobachtung>(ab hier ist alles optional); <Kontakt für Nachfragen> % <nächste Meldung>. Da das Format unübersichtlich und Tippfehleranfällig ist empfehle ich dir lieber das Meldeformular zu nutzen, dort kannst du mir auch Fotos schicken.\n\n#{ALL}: Gibt alle eingegangenen Meldungen aus.\n\n#{COUNT}: Gibt die Gesamtanzahl der bei mir eingegangenen meldungen aus.\n\n#{LAST} <(optional) Tage>: Gibt die Meldungen der letzten 7 Tage aus. Du kannst selber eine Anzahl an Tagen angeben, von denen du die Meldungen sehen möchtest.\n\n#{FEEDBACK}: Gib mir Feedback :) Bewerte mich zuerst auf einer Skala von 1-5 und dann kannst du mir auch noch Tipps geben!", message)
     end
 
+    # Is called when the User Input does not match any known command or step in feedback or reporting
+    #
+    # @param message [Telegram::Bot::Types::Message] the original message received by the bot, used to identify the correct `chat_id` to respond to
     def unknown(message)
         send_message("Sorry, ich weiß nicht was du mir sagen möchtest. Probier doch mal #{HELP}, um zu sehen was ich kann.", message)
     end
